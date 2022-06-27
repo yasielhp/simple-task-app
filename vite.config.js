4e105eeb80b957396dc2d9c37a5e36c7fb07d2ef
Yasiel Hernández Portal <yasielhp@gmail.com>
2022-06-27 12:28:00 +0100

    deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://github.com/yasielken/Simple-Task-App",
+  base: "https://yasielken.github.io/Simple-Task-App",
   plugins: [react()]
 })
