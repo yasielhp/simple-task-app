f1a86cc577ab76baa03902b2a6005d2cb4ce8bb9
Yasiel Hernández Portal <yasielhp@Workstation.local>
2022-08-10 15:06:17 +0100

    deploy in netlify

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://yasielken.github.io/Simple-Task-App",
   plugins: [react()]
 })
